9d93faf82adbdafc8aa8d3889258b62b60c1a3fd
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2023-01-05 19:29:34 +0530

    Implement version base optimistic locking

--- packages/orm/src/typeorm/index.ts
+++ packages/orm/src/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 import { PostgresDriver } from "./driver/PostgresDriver";
 import { GooveeNamingStrategy } from "./naming/GooveeNamingStrategy";
+import { VersionCheck } from "./subscribers/VersionCheck";
 
 export const createDataSource = (options: DataSourceOptions) => {
   const ds = new DataSource({
     ...options,
-    subscribers: [],
+    subscribers: [VersionCheck],
     namingStrategy: new GooveeNamingStrategy(),
   });
 
