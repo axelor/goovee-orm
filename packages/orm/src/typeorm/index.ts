554f739c4b83e843383961aa7adc8067bd313c27
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2023-01-13 12:33:12 +0530

    Remove version check subscriber
    We are now using built-in optimistic lock feature.

--- packages/orm/src/typeorm/index.ts
+++ packages/orm/src/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 import { PostgresDriver } from "./driver/PostgresDriver";
 import { GooveeNamingStrategy } from "./naming/GooveeNamingStrategy";
-import { VersionCheck } from "./subscribers/VersionCheck";
 
 export const createDataSource = (options: DataSourceOptions) => {
   const ds = new DataSource({
     ...options,
-    subscribers: [VersionCheck],
+    subscribers: [],
     namingStrategy: new GooveeNamingStrategy(),
   });
 
