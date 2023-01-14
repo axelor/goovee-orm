dd26880d6e20990d314b306d36de1ad14388cc96
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2023-01-14 15:17:26 +0530

    Don't keep generated test schema in source

--- packages/orm/src/test/entity/AddressType.ts
+++
@@ -1,4 +0,0 @@
-export enum AddressType {
-  Home = "home",
-  Office = "office"
-}
--- packages/orm/src/test/entity/index.ts
+++
@@ -1,7 +0,0 @@
-export { Model } from "./Model";
-export { AddressType } from "./AddressType";
-export { Address } from "./Address";
-export { Circle } from "./Circle";
-export { Contact } from "./Contact";
-export { Country } from "./Country";
-export { Title } from "./Title";
