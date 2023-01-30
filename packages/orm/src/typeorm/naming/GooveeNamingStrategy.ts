2cf1ee3bd01916076621036cb9a5fed6ad5672c0
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2023-01-30 14:27:20 +0100

    Improve axelor naming

--- packages/orm/src/typeorm/naming/GooveeNamingStrategy.ts
+++ packages/orm/src/typeorm/naming/GooveeNamingStrategy.ts
@@ -1,3 +1,6 @@
+import crypto from "node:crypto";
+
+import { Table } from "typeorm";
 import { DefaultNamingStrategy } from "typeorm/naming-strategy/DefaultNamingStrategy";
 import { NamingStrategyInterface } from "typeorm/naming-strategy/NamingStrategyInterface";
 
@@ -49,5 +52,52 @@ export class GooveeNamingStrategy
     inverse?: boolean
   ): string {
     throw new Error("Please provide `inverseJoinColumn` name!");
+  }
+
+  // based on hibernate implementation
+  private generateHashedConstraintName(
+    prefix: string,
+    tableOrName: string | Table,
+    columnNames: string[]
+  ) {
+    const table = tableOrName instanceof Table ? tableOrName.name : tableOrName;
+    const cols = [...columnNames].sort();
+    const names = [`table\`${table}\``, ...cols.map((x) => `column\`${x}\``)];
+
+    const digest = crypto.createHash("md5");
+    const hex = digest.update(names.join()).digest("hex");
+    const num = BigInt(`0x${hex}`);
+    const val = num.toString(35);
+
+    return `${prefix}_${val}`;
+  }
+
+  uniqueConstraintName(tableOrName: string | Table, columnNames: string[]) {
+    return this.generateHashedConstraintName("uk", tableOrName, columnNames);
+  }
+
+  indexName(
+    tableOrName: string | Table,
+    columnNames: string[],
+    where?: string | undefined
+  ) {
+    const table = tableOrName instanceof Table ? tableOrName.name : tableOrName;
+    const names = [table, ...columnNames];
+    return `${names.join("_")}_idx`;
+  }
+
+  foreignKeyName(
+    tableOrName: string | Table,
+    columnNames: string[],
+    _referencedTablePath?: string | undefined,
+    _referencedColumnNames?: string[] | undefined
+  ) {
+    return this.generateHashedConstraintName("fk", tableOrName, columnNames);
+  }
+
+  primaryKeyName(tableOrName: string | Table, columnNames: string[]) {
+    const table =
+      typeof tableOrName === "string" ? tableOrName : tableOrName.name;
+    return `${table}_pkey`;
   }
 }
