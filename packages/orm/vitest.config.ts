2c8bf7d9c1bf66c62abfcbb3d53e9580404f4df0
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2023-02-01 16:08:45 +0100

    Use single vitest.config.ts

--- packages/orm/vitest.config.ts
+++ packages/orm/vitest.config.ts
@@ -1,10 +1,30 @@
+import path from "node:path";
 import swc from "unplugin-swc";
-import { defineConfig,defaultExclude } from "vitest/config";
+import { defaultExclude, defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [swc.vite()],
-  test: {
-    environment: "node",
-    exclude: [...defaultExclude, "src/test"]
-  },
+const unitTests = {
+  environment: "node",
+  exclude: [...defaultExclude, "src/test"],
+};
+
+const e2eTests = {
+  environment: "node",
+  setupFiles: ["src/test/test-setup.ts"],
+  globalSetup: ["src/test/global-setup.ts"],
+  include: ["src/test/*.test.ts"],
+};
+
+export default defineConfig((env) => {
+  return {
+    plugins: [swc.vite()],
+    test: env.mode === "e2e" ? e2eTests : unitTests,
+    resolve: {
+      alias: [
+        {
+          find: "@goovee/orm",
+          replacement: path.join(__dirname, "src"),
+        },
+      ],
+    },
+  };
 });
