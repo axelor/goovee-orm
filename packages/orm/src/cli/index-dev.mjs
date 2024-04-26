c7ca7c9c8eab5ed19fec3a0aa381b5f8615afa59
ame-axelor <localhost+ame-axelor@users.noreply.github.com>
2024-04-26 14:39:00 +0530

    Fix cli in link/workspace mode

--- packages/orm/src/cli/index-dev.mjs
+++ packages/orm/src/cli/index-dev.mjs
@@ -4,39 +4,15 @@ import { spawnSync } from "node:child_process";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
-const script = fileURLToPath(import.meta.url);
-const scriptDir = dirname(script);
+const tsNode = fileURLToPath(import.meta.resolve("ts-node/dist/bin"));
 
-const runSelf = () => {
-  const cwd = dirname(dirname(scriptDir));
-  const cmd = process.env.npm_execpath;
-  const env = {
-    ...process.env,
-    APP_CWD: process.cwd(),
-  };
-  const args = ["ts-node", script, ...process.argv.slice(2)];
-  spawnSync(cmd, args, {
-    env,
-    cwd,
-    stdio: "inherit",
-    windowsHide: true,
-  });
-};
+const scriptDir = dirname(fileURLToPath(import.meta.url));
+const script = join(scriptDir, "index.ts");
 
-const runScript = () => {
-  const cwd = process.env.APP_CWD;
-  const cmd = process.env.npm_node_execpath;
-  const script = join(scriptDir, "index.ts");
-  const args = [process.argv[0], script, ...process.argv.slice(2)];
-  spawnSync(cmd, args, {
-    cwd,
-    stdio: "inherit",
-    windowsHide: true,
-  });
-};
+const cmd = process.argv[0];
+const args = [tsNode, script, ...process.argv.slice(2)];
 
-if (process.env.APP_CWD) {
-  runScript();
-} else {
-  runSelf();
-}
+spawnSync(cmd, args, {
+  stdio: "inherit",
+  windowsHide: true,
+});
